8bc6e597ab733b945dca98fd3c38e40910967c39
Flo <84639717+Florian2807@users.noreply.github.com>
2024-05-12 10:54:55 +0000

    feat: adding language models and handling

---
+++ src/language/language-handler.js
@@ -0,0 +1,14 @@
+module.exports = (filepath, description, variables) => {
+    const filename = filepath.split("/").pop()
+
+    let message = wb.Lang.dict?.[filename]?.[description]
+    if (!message) {
+        console.error('No Message Found: ' + filepath, description)
+        return wb.Lang.handle(__filename, "no_message_found")
+    }
+    
+    for (const key in variables) {
+        message = message.replace(new RegExp('\\${' + key + '}', 'g'), variables[key]);
+    }
+    return message;
+}
