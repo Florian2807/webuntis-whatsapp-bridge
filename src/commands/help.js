b3371c732bac682e73232c27071ea32440a007fc
Flo <84639717+Florian2807@users.noreply.github.com>
2024-08-30 06:27:16 +0000

    help command only for permitted user

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,7 +1,7 @@
 module.exports = {
 	commandName: 'help',
 	triggers: ['hilfe'],
-	onlyPermittedUser: false,
+	onlyPermittedUser: true,
 	needTeacherAccess: false,
 	callback: ({ msg }) => {
 		return msg.reply(wb.Lang.handle(__filename, 'output_message', { outputData: wb.config.helpURL}));
