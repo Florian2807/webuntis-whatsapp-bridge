2fa6b1b32869ceeac007069abb807089f532e4da
Flo <84639717+Florian2807@users.noreply.github.com>
2024-11-18 13:28:27 +0000

    fix: check user auth

--- src/utils/checkUserAuth.js
+++ src/utils/checkUserAuth.js
@@ -4,7 +4,7 @@ module.exports = async (checkingUser) => {
     const authorizedUsers = JSON.parse(process.env.whatsapp_admins);
     for (const c of wb.config.classes) {
         if (c.whatsapp_groupID.includes("@g.us")) {
-            const group = (await client.getChats()).find(c => c.id === c.whatsapp_groupID);
+            const group = (await wb.Whatsapp.getChats()).find(c => c.id === c.whatsapp_groupID);
             console.log(group.participants);
             authorizedUsers.push(...group.participants.filter(i => i.id.server === 'c.us').map(participant => participant.id._serialized));
         } else {
