c89e0361b369801faba88661cf8d7f9713698b6f
Flo <84639717+Florian2807@users.noreply.github.com>
2024-11-18 13:33:03 +0100

    chore: remove logs

--- src/utils/getTimetableData.js
+++ src/utils/getTimetableData.js
@@ -23,7 +23,6 @@ module.exports = async ({ classID, date }) => {
 		console.log(new Date().toLocaleTimeString('de') + JSON.stringify(body));
 		return wb.Utils.getTimetableData({ classID, date });
 	}
-	console.log('x')
 	const { data } = body;
 
 	const timetable = {};
