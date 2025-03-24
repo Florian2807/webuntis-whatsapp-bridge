8b03192e1e5456eaf3a094e5632eada9f9456993
Flo <84639717+Florian2807@users.noreply.github.com>
2025-03-24 07:46:43 +0000

    xd test

--- clients/webuntis.js
+++ clients/webuntis.js
@@ -3,3 +3,5 @@ const { WebUntis } = require('webuntis');
 const client = new WebUntis(process.env['untis_school'], process.env['untis_username'], process.env['untis_password'], process.env['untis_baseurl']);
 client.login();
 module.exports = client;
+
+console.log('asd')
